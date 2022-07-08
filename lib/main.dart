cde9ba651f0074d36729335c0d565a385594eaa5
yamoto0628 <a18sb009@zr.osaka-cu.ac.jp>
2022-07-08 18:29:16 +0900

    create simple chat app

--- lib/main.dart
+++ lib/main.dart
@@ -4,112 +4,139 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ChatMessageModel extends StatelessWidget {
+  final String avatarUrl; //ユーザのアイコンURL
+  final String name; //名前
+  final String datetime; //メッセージの日時
+  final String message; //メッセージ本文
 
-  // This widget is the root of your application.
+  ChatMessageModel(this.name, this.datetime, this.message, this.avatarUrl);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Card(
+      child: ListTile(
+        title: Text(message),
+        subtitle: Row(
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: NetworkImage(avatarUrl),
+              radius: 10.0,
+            ),
+            Text(
+              datetime,
+            ),
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class BottomMessageArea extends StatelessWidget {
+  BottomMessageArea({
+    super.key,
+    required this.onPressed,
+  });
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final Function(ChatMessageModel) onPressed;
+  final valueController = TextEditingController();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  void sendMessage() {
+    final messageWid = ChatMessageModel(
+        "yamoyamoto",
+        "now",
+        valueController.text,
+        "https://user-images.githubusercontent.com/68677002/177938648-6afb1c7d-a473-42f3-a6a0-b54dd0da7bee.png");
+    onPressed(messageWid);
+  }
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextFormField(
+            controller: valueController,
+            keyboardType: TextInputType.multiline,
+            maxLines: 5,
+            minLines: 1,
+            decoration: const InputDecoration(
+              hintText: 'メッセージを入力してください!',
+            ),
+          ),
+        ),
+        IconButton(
+            onPressed: () {},
+            icon: IconButton(
+              onPressed: sendMessage,
+              icon: Icon(Icons.send),
+            ))
+      ],
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MessageList extends StatefulWidget {
+  const MessageList({super.key});
 
-  void _incrementCounter() {
+  @override
+  State<StatefulWidget> createState() => _MessageListState();
+}
+
+class _MessageListState extends State<MessageList> {
+  List<ChatMessageModel> messages = [];
+
+  void _add(ChatMessageModel message) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      messages.add(message);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return ListView(
+      children: messages,
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  List<ChatMessageModel> messageData = [
+    ChatMessageModel("yamoto", "2022-01-01 00:00:00", "Hello!",
+        "https://user-images.githubusercontent.com/68677002/177938648-6afb1c7d-a473-42f3-a6a0-b54dd0da7bee.png"),
+    ChatMessageModel("yamoto", "2022-01-01 10:00:00", "Hoge!",
+        "https://user-images.githubusercontent.com/68677002/177938648-6afb1c7d-a473-42f3-a6a0-b54dd0da7bee.png"),
+  ];
+
+  void _addMessage(ChatMessageModel message) {
+    setState(() {
+      messageData.add(message);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: Stack(
+          alignment: Alignment.bottomCenter,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ListView(children: messageData),
+            BottomMessageArea(
+              onPressed: _addMessage,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
